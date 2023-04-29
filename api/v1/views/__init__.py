9a21895db0669f388a609a32bca31b18f0e31e68
Yosinan <ymymym4634@gmail.com>
2023-04-29 04:09:57 -0700

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,10 @@
 '''
     Views BluePrint
 '''
-from api.v1.views.index import *
+
 from flask import Blueprint
 
 '''a variable app_views which is an instance of Blueprint '''
+
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+from api.v1.views.index import *
