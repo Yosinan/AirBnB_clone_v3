fc9ec9fe1941174e0f896c51d285cba51b05de4d
Yosinan <ymymym4634@gmail.com>
2023-04-29 06:52:46 -0700

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 '''
     Views BLuePrint
 '''
-from flask import Flask, jsonify
+from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
