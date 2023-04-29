b315a007e1d115ae2ad983562678e9b5aff4d8d0
Yosinan <ymymym4634@gmail.com>
2023-04-29 06:34:11 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
     start a Flask Web-App
 '''
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from os import getenv
 from api.v1.views import app_views
 from models import storage
@@ -18,7 +18,16 @@ def db_teardown(exception):
     storage.close()
 
 
+@app.errorhandler(404):
+    '''
+    404 response in JSON format
+    '''
+    return make_response(jsonify({'Error: 'Not found'}), 404)
+
+
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST')
     port = getenv('HBNB_API_PORT')
-    app.run(host or '0.0.0.0',port or 5000)
+    app.run(host or '0.0.0.0',
+            port or 5000,
+            threaded=True)
