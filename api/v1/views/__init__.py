290f1d99c26938d60fec511e09097c66a18b1cae
Yosinan <ymymym4634@gmail.com>
2023-04-29 04:01:24 -0700

    api

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+    Views BluePrint
+'''
+from flask import Blueprint
+
+'''a variable app_views which is an instance of Blueprint '''
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+    Views BLuePrint 
+'''
+from flask import Flask
+from api.v1.views import app_views
+
+
+@app_views.route('/status', strict_slashes=False)
+def statusApi():
+    '''
+    returns a JSON: "status": "OK"
+    '''
+    return jsonify(status="OK")
