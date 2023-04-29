ec4011256575e9371db599baab14db31f6675752
Yosinan <ymymym4634@gmail.com>
2023-04-29 06:14:12 -0700

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,16 @@ def statusApi():
     returns a JSON: "status": "OK"
     '''
     return jsonify(status="OK")
+
+
+@app_views.route("/stats", strict_slashes=Flase)
+def stats_api():
+    '''
+    /stats endpoint will return JSON formatted stats on all data
+    '''
+    stats = {"amenities": Amenity, "cities": City, "places": Place, "reviews": Review, "states": State, "user": User }
+
+    for s in stats:
+        stats[s] = storage.count(stats[s])
+
+    return jsonify(stats)
