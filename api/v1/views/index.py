93ea1d3846230af30284ba17db4cec6131b77952
Yosinan <ymymym4634@gmail.com>
2023-04-29 04:14:50 -0700

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from flask import Flask
 from api.v1.views import app_views
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def statusApi():
     '''
     returns a JSON: "status": "OK"
