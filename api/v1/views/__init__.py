82f1f07325c4e87aa5bd883a9e1feff38d3cce37
yordigenet <yrdigent@gmail.com>
2023-04-30 00:02:01 -0700

    fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,7 @@ from flask import Blueprint
 '''a variable app_views which is an instance of Blueprint '''
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
